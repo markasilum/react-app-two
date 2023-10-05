bbf8298dc7eba0de31a3f0428da84dba74050c1a
markasilum <markasilum13@gmail.com>
2023-10-05 19:06:05 +0800

    added button

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className='p-4 bg-white border border-gray-400 w-[250px] text-center'>
+        <h4>Title</h4>
+        <p>Body</p>
+    </div>
+  )
+}
+
+export default Post
