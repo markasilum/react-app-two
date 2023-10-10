a6804d23d7f10da201460eaa89c4b9133de6aec5
markasilum <markasilum13@gmail.com>
2023-10-10 18:43:46 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Post from './components/Post'
 
 function App() {
+  const[number, setNumber] = useState(1)
+  const[post, setPost] = useState(null)
+  
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts/'+number)
+    .then((response)=> response.json())
+    .then((data)=> setPost(data))
+    .catch((error)=>console.log(error))
+  },[number])
+
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center'>
-      <Post />
+      {post && <Post title={post.title} body={post.body}/>}
+      {number}
       <div className='flex items-center gap-3 mt-3'>
         {Array.from({length:5}).map((value, index) => {
           return(
-            <div className='p-4 border font-bold hover:cursor-auto' >
-              {index}
+            <div key={`pagination-${index+1}`} className='p-4 border font-bold hover:cursor-auto' onClick={()=>setNumber(index+1)}>
+              {index+1}
             </div>
           )
         })}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({title, body}) => {
   return (
     <div className='p-4 bg-white border border-gray-400 w-[250px] text-center'>
-        <h4>Title</h4>
-        <p>Body</p>
+        <h4 className='font-bold'>{title}</h4>
+        <p>{body}</p>
     </div>
   )
 }
