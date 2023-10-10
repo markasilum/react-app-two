fcefe618d50ed9b80614ae1a1c5836ea865955d4
markasilum <markasilum13@gmail.com>
2023-10-10 19:23:24 +0800

    added hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { useEffect, useState } from 'react'
 import Post from './components/Post'
+import useFetch from './hooks/useFetch'
 
 function App() {
   const[number, setNumber] = useState(1)
-  const[post, setPost] = useState(null)
+  // const[post, setPost] = useState(null)
 
-  const fetchPost = async (id) => {
-    try{
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts/'+id)
-      const data = await response.json()
-      setPost(data)
-    }catch(error){
-      console.log(error)
-    }finally{
-      console.log('Done')
-    }
-  }
+  const {data} = useFetch('https://jsonplaceholder.typicode.com/posts/', number)
 
-  useEffect(()=>{
-    // fetch('https://jsonplaceholder.typicode.com/posts/'+number)
-    // .then((response)=> response.json())
-    // .then((data)=> setPost(data))
-    // .catch((error)=>console.log(error))
-    fetchPost(number)
-  },[number])
+  // useEffect(()=>{
+  //   // fetch('https://jsonplaceholder.typicode.com/posts/'+number)
+  //   // .then((response)=> response.json())
+  //   // .then((data)=> setPost(data))
+  //   // .catch((error)=>console.log(error))
+  //   fetchPost(number)
+  // },[number])
 
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center'>
-      {post && <Post title={post.title} body={post.body}/>}
+      {data && <Post title={data.title} body={data.body}/>}
       {number}
       <div className='flex items-center gap-3 mt-3'>
         {Array.from({length:5}).map((value, index) => {
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+const useFetch = (url, id) => {
+    const[data, setData] = useState(null)
+    const fetchData = async (id) => {
+        try{
+          const response = await fetch(url+id)
+          const content = await response.json()
+          setData(content)
+        }catch(error){
+          console.log(error)
+        }finally{
+          console.log('Done')
+        }
+      }
+
+      useEffect(()=>{
+        fetchData(id)
+      },[id])
+
+      return{
+        data
+      }
+}
+
+
+
+
+
+export default useFetch
